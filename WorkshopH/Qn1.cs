50eacd637dad9b8c82e8bdaa84509cd693403c0f
knghtedbiiscuit <alibiscuit@gmail.com>
2017-02-10 17:24:52 +0800

    Add project files.

---
+++ WorkshopH/Qn1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WorkshopH
+{
+    class Qn1
+    {
+        static void Main()
+        {
+            string[] s1 = new string [] { "The brown fox", "The brown fox", "The brown fox", "T", "The bras basah complex"};
+            string[] s2 = new string [] {"O","FOX","bRO", "bRO", "bRO"};
+
+            int length = s1.Length; 
+
+            for (int i = 0; i < length; i++)
+            {
+                    Console.WriteLine(InString(s1[i], s2[i]));
+            }
+
+        }
+
+
+        // This method check if Sting S2 is a subset of String S1
+        static bool InString(string s1, string s2)
+        {
+        return true;
+        }
+    }
+}
